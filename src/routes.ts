fb7301b2fd323bb9db2ea51cfe2b94123f9ac8e8
christopher-estanis <christopher.estanis@gmail.com>
2021-06-07 20:20:05 -0300

    change tsConfig

--- src/routes.ts
+++ src/routes.ts
@@ -7,39 +7,13 @@ const routes = Router()
 const fs = require('fs')
 
 routes.get("/ping", (request, response) => {
-  response.json("pingou")
+  return response.json("pingou")
 })
 
 routes.post("/image", upload.single('image'), async (request, response) => {
   const filepath = request.file.path
 
   return response.send('aaaaa')
-  try {
-    const worker = createWorker({
-      langPath: 'tmp/traineddata',
-      gzip: false
-    })
-
-    await worker.load()
-    await worker.loadLanguage('por')
-    await worker.initialize('por')
-    const { data: { text } } = await worker.recognize(filepath)
-    await worker.terminate()
-
-    fs.unlink(filepath, (error) => {
-      if (error)
-        console.error(error)
-    })
-
-
-    return response.status(200).send(text)
-  } catch (error) {
-    fs.unlink(filepath, (error) => {
-      if (error)
-        console.error(error)
-    })
-    return response.status(400).json(error)
-  }
 
 })
 
